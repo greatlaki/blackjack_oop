a6357679b7472f67a41b0c31ba495eeaed242954
greatlaki <greatlaki911@gmail.com>
2022-02-05 15:31:25 +0300

    I have finished writing the code in the file deck.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    pass
+from deck import Deck
+
+if __name__ == '__main__':
+    d = Deck()
+
+    print(len(d))
+    card = d.get_card()
+    print(card)
+    print(len(d))
+
+
