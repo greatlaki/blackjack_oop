8a91aa92b71432b44e2da6532d28377cd4a5685a
greatlaki <greatlaki911@gmail.com>
2022-02-05 15:54:49 +0300

    Iniinitialized parent class AbstractPlayer

--- player.py
+++ player.py
@@ -1,2 +1,23 @@
-class Player:
+import abc
+
+
+class AbstractPlayer(abc.ABC):
+
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def ask_card(self):
+        pass
+
+
+class Player(AbstractPlayer):
+    pass
+
+
+class Dealer(AbstractPlayer):
+    pass
+
+
+class Bot(AbstractPlayer):
     pass
